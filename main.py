ae8813784f5429677ccac260575ee457217ce6aa
kurt1s <knbsk8@gmail.com>
2019-03-17 23:05:30 -0400

    Added BTC price, quote of the day, and countdown functions

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
+import requests
+from babel.numbers import format_currency
 
-TOKEN = 'NDkxNDA3MzU4ODIwNDgzMDc0.D3A1gg.2phYPvETPzMs4smj2CqO9X5Avl4'
+
+TOKEN = 'NTU3MDEyMTM4MDQ5NDA0OTI4.D3CFxQ.llBOmngDv4c0FHeJQffsM4RcKUA'
 
 client = discord.Client()
 
@@ -18,10 +21,50 @@ async def on_message(message):
         msg = 'Goodbye {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-    if message.content.startswith('!play'):
-        msg = 'Ok I will play some music for you'.format(message)
-        await client.send_message(message.channel,msg)
-        await client.send_typing(message.channel,msg)
+    # if message.content.startswith('!play'):
+    #     msg = 'Ok I will play some music for you'.format(message)
+    #     await client.send_message(message.channel, msg)
+
+    # Kurtis
+    if message.content.startswith('!info'):
+        info = client.application_info()
+        msg = info
+        await client.send_message(message.channel, msg)
+
+    # Kurtis
+    if message.content.startswith('!321'):
+        await client.send_message(message.channel, "3\n2\n1\nTime's up!", tts=True)
+
+    # Kurtis
+    if message.content.startswith('!quote'):
+        url = "http://www.forbes.com/forbesapi/thought/uri.json?enrich=true&query=1&relatedlimit=5"
+        response = requests.get(url)
+        data = response.json()
+        quote = data['thought']["quote"]
+
+        await client.send_message(message.channel, quote.format(message), tts=True)
+
+    # Kurtis
+    if message.content.startswith('!btc'):
+        url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+        response = requests.get(url)
+        data = response.json()
+        price = data['bpi']['USD']['rate']
+        # format_currency(float(price), 'USD', locale='en_US')
+        await client.send_message(message.channel, price, tts=True)
+
+
+    # Kurtis
+    if message.content.startswith('!eth'):
+        url = "https://api.coinmarketcap.com/v1/ticker/ethereum/"
+        response = requests.get(url)
+        data = response.json()
+        price = data[0]['price_usd']
+        # format_currency(float(price), 'USD', locale='en_US')
+        await client.send_message(message.channel, price, tts=True)
+
+
+
 
 @client.event
 async def on_ready():
